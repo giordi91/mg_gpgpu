545ab0eeab80e4f53b1172bb66a5957bab72f159
giordi <giordi@ant-17.lan>
2017-01-27 19:05:41 +1100

    arse version of parlalle_reduce, just to get some cuda going and cmake linking cuda

---
+++ mg_gpgpu_core/include/mg_gpgpu_core/parallel_reduce.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+
+    
+unsigned int parallel_reduce( unsigned int* host_data, unsigned int element_count);
+
--- tests/dummyTests.cpp
+++ tests/dummyTests.cpp
@@ -1,6 +1,8 @@
 #include <gmock/gmock.h>
 #include <iostream>
 #include <mg_gpgpu_core/dummy.h>
+#include <mg_gpgpu_core/parallel_reduce.h>
+#include <vector>
 using namespace testing;
 
 
@@ -8,4 +10,16 @@ using namespace testing;
 TEST(Attribute_test, basic_instances)
 {
 	test();
+}
+
+TEST(Attribute_test, cuda_run)
+{
+    std::vector<unsigned int> vec; 
+    unsigned int size (1024*1024);
+    vec.resize(size);
+    for (int i =0; i <size;++i)
+    {
+        vec[i] = i; 
+    }
+	parallel_reduce(vec.data(), vec.size());
 }
