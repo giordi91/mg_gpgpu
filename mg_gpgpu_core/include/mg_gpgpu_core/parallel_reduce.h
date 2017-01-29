07e1393aa96a8e4ee72a77ed9ac22fd0a43c505a
Marco Giordano <marco.giordano.work@gmail.com>
2017-01-29 17:06:54 +1100

    optimized memory foot print for parallel_reduce regular kernel added some extra tests to tests execution of not power of two element counts, commented a bit the code

--- mg_gpgpu_core/include/mg_gpgpu_core/parallel_reduce.h
+++ mg_gpgpu_core/include/mg_gpgpu_core/parallel_reduce.h
@@ -5,34 +5,57 @@
 
     
 template <typename T>
-__global__ void global_reduce_kernel(T * d_out, T * d_in, unsigned int count)
+__global__ void parallel_reduce_kernel(T * d_out, T * d_in, unsigned int count)
 {
+    //this is a pretty off the shelf cuda impelemntation of a reduce kernel,
+    //here we use a unsigned char shared_data and then cast the value, due to the 
+    //fact that cuda won't let me redefined the shared memory for some reason.
 	extern __shared__ __align__(sizeof(T)) unsigned char shared_data[];
     T *sdata= reinterpret_cast<T *>(shared_data);
 
+    //computing global index thread and index inside the block
     int myId = threadIdx.x + blockDim.x * blockIdx.x;
     int tid  = threadIdx.x;
 
+    //here we load the memory from global to shared, we take extra care
+    // in handling the boundary condition since the following for loop
+    // will loop over the whole block, no matter if used or not
     if (myId >= count)
-    { sdata[tid] = 0.0;}
+    { sdata[tid] = static_cast<T>(0);}
     else
+    { sdata[tid]= d_in[myId]; }
+
+    //here we syncronize the thread in a block wise fashion making sure 
+    //the data has been loaded for the whole block
+    __syncthreads();        
+    
+    //here we trigger the reduction, we start with a block wide dimension 
+    //and we proceed in a log(blockDim.x) fashion, halving the size at 
+    //each iteration
+    for (unsigned int block_width = blockDim.x / 2;block_width > 0; block_width >>= 1)
     {
-        sdata[tid]= d_in[myId];
-    }
-    __syncthreads();        // make sure all adds at one stage are done!
-    // do reduction in global mem
-    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-    {
-        if (tid < s)
+        //here we need to mask out the threads that actually need
+        //to perform the operation, since we sum the higher part of the 
+        //block to the lower part of the block, half of the threads won't be 
+        //working
+        if (tid < block_width)
         {
-            sdata[tid] += sdata[tid + s];
+            sdata[tid] += sdata[tid +block_width ];
         }
-        __syncthreads();        // make sure all adds at one stage are done!
+        //since we are working with global memory we always need to performa
+        //a block wise syncronization to make sure not incur in race conditions
+        __syncthreads();        
     }
 
-    // only thread 0 writes result for this block back to global mem
+    //at this point the block has been reduced in the threadIdx.x == 0,
+    //so we let the first thread write to memory, doesnt necessarly have 
+    //to be thread zero, the key thing is that only one thread per block
+    //writes to shared memory
     if (tid == 0)
     {
+        //here we use the block index to write to global memory, this mean
+        //we eneded up with blockCount data written, where block count is the 
+        //total number of blocks in the kernel
         d_out[blockIdx.x] = sdata[tid];
     }
 }
@@ -41,7 +64,6 @@ __global__ void global_reduce_kernel(T * d_out, T * d_in, unsigned int count)
 template<typename T>
 T parallel_reduce( T* host_data, unsigned int element_count)
 {
-
     unsigned int block_size = 1024;
     T* device_data;
     T* out_device_data;
@@ -53,7 +75,10 @@ T parallel_reduce( T* host_data, unsigned int element_count)
    
 
     cudaMalloc( (void**)&device_data, size );
-    cudaMalloc( (void**)&out_device_data, size );
+    //we know already how many blocks we are gonna kick in the first iteration
+    //so we don't have to allocate the full array for the output data,
+    //we can get away with block count elements
+    cudaMalloc( (void**)&out_device_data, blocks*sizeof(T));
     cudaMemcpy( device_data , host_data, size, cudaMemcpyHostToDevice );
 
     T * in = out_device_data;
@@ -66,10 +91,8 @@ T parallel_reduce( T* host_data, unsigned int element_count)
         unsigned int grid_size = ((i%block_size) != 0)?(ratio) +1: (ratio);
         dim3 dimBlock( block_size, 1 );
         dim3 dimGrid( grid_size, 1 );
-        global_reduce_kernel<T><<<grid_size, block_size, sizeof(T)*block_size>>>( out, in, i);
+        parallel_reduce_kernel<T><<<grid_size, block_size, sizeof(T)*block_size>>>( out, in, i);
         i = grid_size;
-       
-       
     }
     T result;
     cudaMemcpy(&result, out,sizeof(T), cudaMemcpyDeviceToHost);
@@ -79,4 +102,31 @@ T parallel_reduce( T* host_data, unsigned int element_count)
     return result;
 
 }
+
+
+//template <typename T>
+//__global__ void global_reduce_kernel_shuffle(T * d_out, T * d_in, unsigned int count)
+//{
+//    int myId = threadIdx.x + blockDim.x * blockIdx.x;
+//    int tid  = threadIdx.x;
+//
+//    // do reduction in global mem
+//	T value =0;
+//    for (unsigned int b = blockDim.x / 2; b > 0; b >>= 1)
+//    {
+//        if (tid < b)
+//        {
+//            for (unsigned int s = 32 / 2; s > 0; s >>= 1)
+//            {
+//                value +=  __shfl_down(value,s );
+//            }
+//        }
+//    }
+//
+//    // only thread 0 writes result for this block back to global mem
+//    if (tid == 0)
+//    {
+//        d_out[blockIdx.x] = sdata[tid];
+//    }
+//}
 
--- tests/parallel_reduce_test.cu
+++ tests/parallel_reduce_test.cu
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace testing;
 
-TEST(cuda_parallel_reduce,integer_test )
+TEST(cuda_parallel_reduce,integer_numbers_from_1_to_n)
 {
     std::vector<unsigned int> vec; 
     unsigned int size (1024);
@@ -15,10 +15,24 @@ TEST(cuda_parallel_reduce,integer_test )
     }
 	unsigned int res = parallel_reduce<unsigned int>(vec.data(), vec.size());
 	auto math_res = (size*(size +1) *0.5);
-    ASSERT_TRUE((res )== math_res );
+    ASSERT_EQ(res , math_res );
 }
 
-TEST(cuda_parallel_reduce,float_test)
+TEST(cuda_parallel_reduce,integer_numbers_from_1_to_n_not_power_of_2)
+{
+    std::vector<unsigned int> vec; 
+    unsigned int size (1300);
+    vec.resize(size);
+    for (int i =0; i <size;++i)
+    {
+        vec[i] = i+1; 
+    }
+	unsigned int res = parallel_reduce<unsigned int>(vec.data(), vec.size());
+	auto math_res = (size*(size +1) *0.5);
+    ASSERT_EQ(res , math_res );
+}
+
+TEST(cuda_parallel_reduce,float_numbers_from_1_to_n)
 {
     std::vector<float> vec; 
     unsigned int size (1024);
@@ -32,4 +46,40 @@ TEST(cuda_parallel_reduce,float_test)
 	float res= parallel_reduce<float >(vec.data(), vec.size());
 	float math_res = (size*(size +1) *0.5);
     ASSERT_FLOAT_EQ(res , math_res );
+}
+
+TEST(cuda_parallel_reduce,float_numbers_random)
+{
+    std::vector<float> vec; 
+    unsigned int size (1024);
+    vec.resize(size);
+
+	float accum=0;
+    for (int i =0; i <size;++i)
+    {
+        auto value = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		vec[i] = value;
+		accum += value;
+    }
+
+	float res= parallel_reduce<float >(vec.data(), vec.size());
+    ASSERT_FLOAT_EQ(res , accum );
+}
+
+TEST(cuda_parallel_reduce,float_numbers_random_not_power_of_2)
+{
+    std::vector<float> vec; 
+    unsigned int size (1121);
+    vec.resize(size);
+
+	float accum=0;
+    for (int i =0; i <size;++i)
+    {
+        auto value = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+		vec[i] = value;
+		accum += value;
+    }
+
+	float res= parallel_reduce<float >(vec.data(), vec.size());
+    ASSERT_FLOAT_EQ(res , accum );
 }
